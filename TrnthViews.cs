378989018c6d3fa70daf9a6c4bbdba3e9164e1d6
Jimbow <adeathjohn@gmail.com>
2014-07-18 12:56:27 +0800

    sdfas

---
+++ TrnthViews.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+public class TrnthViews : TrnthMonoBehaviour {
+	public int width;
+	public int height;
+	[ContextMenu ("execute")]
+	public void execute(){
+		foreach(Transform e in transform){
+			var widget=e.GetComponent<UIWidget>();
+			if(!widget)continue;
+			widget.SetDimensions(width,height);
+			// var vec=widget.drawRegion;
+			// vec.z=width;
+			// vec.w=height;
+			// widget.drawRegion=vec;
+			// Debug.Log("ddd");
+		}
+	}
+}
