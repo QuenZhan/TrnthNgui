4fa55283dd621c91bd963a4ddf039e738a4c2f24
Jimbow <adeathjohn@gmail.com>
2014-07-18 11:49:47 +0800

    dfsf

---
+++ TrnthProxySlider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+public class TrnthProxySlider : TrnthMonoBehaviour {
+	// public TrnthRadio radio;
+	public UILabel label;
+	public UISlider slider;
+	public bool floor;
+	public float start;
+	public float end;
+	public float value;
+	public void execute(){
+		value=Mathf.Lerp(start,end,slider.value);
+		// radio.rate=;
+		if(floor)value=Mathf.Floor(value);
+		label.text=value+"";
+	}
+	void Update(){
+		execute();
+	}
+}
