42bcbb45a9f82ce95de729b37b4c851992ab6d52
Jim Mac mini <jimzheng@voo.com.tw>
2014-08-06 18:53:14 +0800

    cache added

--- TrnthUiTextureNetwork.cs
+++ TrnthUiTextureNetwork.cs
@@ -1,21 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TrnthUiTextureNetwork : MonoBehaviour {
+    static public Dictionary<string,Texture> cache=new Dictionary<string,Texture>();
 	public UITexture uitex;
-	public bool has=false;
-	public string url;
+	public Texture2D texLoading;
+    public bool cacheUsing=true;
 	public void texWithUrl(string url){
 		this.url=url;
-		if(gameObject.activeInHierarchy)StartCoroutine(textureWithUrl(url));
-		else has=false;
+        has=false;
+        if(cacheUsing&&cache.ContainsKey(url)){
+            uitex.mainTexture=cache[url];
+            has=true;
+        }else if(gameObject.activeInHierarchy)StartCoroutine(textureWithUrl(url));
 	}
+	string url;
+	bool has=false;
 	IEnumerator textureWithUrl(string url) {
         WWW www = new WWW(url);
+        if(texLoading)uitex.mainTexture=texLoading;
+
         yield return www;
         
-        if(www.error!=null)Debug.Log(www.error);
-        else  uitex.mainTexture = www.texture;
+        if(www.error!=null){
+        	// Debug.Log(www.error);
+        }
+        else  {
+            uitex.mainTexture = www.texture;
+            cache[url]=www.texture;
+        }
         has=true;
     }
     void OnEnable(){
