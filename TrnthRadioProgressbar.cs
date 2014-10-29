2e2caf6db0e0e7e6699ef73ef8018e015014c599
JimbowMBPR <adeathjohn@gmail.com>
2014-10-29 16:50:19 +0800

    adfsf

---
+++ TrnthRadioProgressbar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthRadioProgressbar : TrnthRadio {
+	public UIProgressBar bar;
+	public string uiProgressBar;
+	// Update is called once per frame
+	void Start(){
+		if(!bar){
+			var ggg=GameObject.Find(uiProgressBar);
+			if(ggg)bar=ggg.GetComponent<UIProgressBar>();
+		}
+	}
+	void Update () {
+		if(bar)bar.value=rate;
+	}
+}
