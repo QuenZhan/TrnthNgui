226232f668b8fd87424afdcc3e2411afc9c87091
JimbowMBPR <adeathjohn@gmail.com>
2014-11-12 12:39:27 +0800

    no message

---
+++ TrnthRandomAlphaWidget.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthRandomAlphaWidget : MonoBehaviour {
+	public UIWidget widget;
+	public float value;
+	public float noise;
+	public void execute(){
+		widget.alpha=value+Random.value*noise;
+	}
+	void OnEnable () {
+		execute();
+	}
+}
