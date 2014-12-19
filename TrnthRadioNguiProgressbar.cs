eea5df7a7cac8af21367fdfd17bfb671d4a58bd1
JimbowMBPR <adeathjohn@gmail.com>
2014-12-20 04:34:54 +0800

    asdfsa

--- TrnthRadioNguiProgressbar.cs
+++ TrnthRadioNguiProgressbar.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class TrnthRadioNguiProgressbar : TrnthRadio {
 	public UIProgressBar bar;
 	public string uiProgressBar;
-	// Update is called once per frame
-	void Start(){
+	public void find(){
 		if(!bar){
 			var ggg=GameObject.Find(uiProgressBar);
 			if(ggg)bar=ggg.GetComponent<UIProgressBar>();
 		}
+	}
+	// Update is called once per frame
+	void Start(){
+		find();
 	}
 	void Update () {
 		if(bar)bar.value=rate;
