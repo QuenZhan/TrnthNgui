ee17203995a43d016e7e8096b9cc5938494bd59f
JimbowMBPR <adeathjohn@gmail.com>
2014-12-25 23:54:04 +0800

    asdf

---
+++ TrnthHVSActionNguiWidgetFitScreen.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthHVSActionNguiWidgetFitScreen : TrnthHVSAction {
+	public UIWidget widget;
+	protected override void _execute() {
+		if(!widget)widget=GetComponent<UIWidget>();
+		widget.width=Screen.width;
+		widget.height=Screen.height;
+	}
+}
