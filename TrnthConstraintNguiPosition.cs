fafe560f08ec1479ebd63e9e9855e71bc3a1c504
JimbowMBPR <adeathjohn@gmail.com>
2015-02-04 02:15:41 +0800

    sdf

---
+++ TrnthConstraintNguiPosition.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthConstraintNguiPosition : TrnthConstraint {
+	public Transform alignTo;
+	// public Camera mainCamera;
+	public override void update(){
+		base.update();
+		var coor=Camera.main.WorldToScreenPoint(alignTo.position);
+		target.localPosition=new Vector3(coor.x-Screen.width*0.5f,coor.y-Screen.height*0.5f,0);
+	}
+}
