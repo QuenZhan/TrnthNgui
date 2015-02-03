e554e42b1583a4292a35d785a8c6d056f15bc653
JimbowMBPR <adeathjohn@gmail.com>
2015-02-04 07:25:50 +0800

    no message

--- TrnthConstraintNguiPosition.cs
+++ TrnthConstraintNguiPosition.cs
@@ -6,6 +6,10 @@ public class TrnthConstraintNguiPosition : TrnthConstraint {
 	// public Camera mainCamera;
 	public override void update(){
 		base.update();
+		if(!alignTo){
+			Destroy(this);
+			return;
+		}
 		var coor=Camera.main.WorldToScreenPoint(alignTo.position);
 		target.localPosition=new Vector3(coor.x-Screen.width*0.5f,coor.y-Screen.height*0.5f,0);
 	}
