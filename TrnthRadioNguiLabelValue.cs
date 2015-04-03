7362bcd8c8c1a0b6e5b433f44604d6d6f8aa9666
JImbowMBAir <adeathjohn@gmail.com>
2015-04-04 00:24:10 +0800

    dsf

--- TrnthRadioNguiLabelValue.cs
+++ TrnthRadioNguiLabelValue.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class TrnthRadioNguiLabelValue : TrnthRadio {
 	public UILabel label;
 	void Update(){
+		if(!label){
+			enabled=false;
+			return;
+		}
 		label.text=value+"";
 	}
 }
