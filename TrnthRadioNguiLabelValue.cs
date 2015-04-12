0b8533d21a1b5c9425c76194b0822abd007ec86f
JimbowMBPR <adeathjohn@icloud.com>
2015-04-12 11:52:37 +0800

    member ui

--- TrnthRadioNguiLabelValue.cs
+++ TrnthRadioNguiLabelValue.cs
@@ -5,7 +5,8 @@ public class TrnthRadioNguiLabelValue : TrnthRadio {
 	public UILabel label;
 	void Update(){
 		if(!label){
-			enabled=false;
+			// enabled=false;
+			Debug.Log("if(!label)",transform);
 			return;
 		}
 		label.text=value+"";
