8e066ce5fef3d3f3b11878159cc1f78977d86098
JimbowMBPR <adeathjohn@icloud.com>
2015-04-15 17:15:26 +0800

    dialoge , stage intro

---
+++ TrnthHVSActionDialougeNguiLabel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthHVSActionDialougeNguiLabel : TrnthHVSAction {
+	public TrnthDialogueManager dialogueManager;
+	public UILabel label;
+	protected override void _execute(){
+		base._execute();
+		label.text=dialogueManager.dialogue.text;
+	}
+}
