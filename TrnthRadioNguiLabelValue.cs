99e94afdf4fa9dbd35a96923d157d1de792d1787
JimbowMBPR <adeathjohn@icloud.com>
2015-06-04 23:49:57 +0800

    project setting

--- TrnthRadioNguiLabelValue.cs
+++ TrnthRadioNguiLabelValue.cs
@@ -5,7 +5,6 @@ public class TrnthRadioNguiLabelValue : TrnthRadio {
 	public UILabel label;
 	void Update(){
 		if(!label){
-			// enabled=false;
 			Debug.Log("if(!label)",transform);
 			return;
 		}
